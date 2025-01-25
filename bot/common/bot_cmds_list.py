b7c7dd96f08259623ed2756e80365c63dfb547d3
Kirill Kononenko <kononenkokireall@gmail.com>
2025-01-25 13:27:30 +0100

    Ready-made laayout for bot processing

--- bot/common/bot_cmds_list.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import BotCommand
-
-private = [
-    BotCommand(command='menu', description='Просмотреть меню'),
-    BotCommand(command='about', description='О нас'),
-    BotCommand(command='payment', description='Варианты оплаты'),
-    BotCommand(command='shipping', description='Варианты доставки'),
-]
