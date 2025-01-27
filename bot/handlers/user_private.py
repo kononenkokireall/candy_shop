706ece76cb04d3aafa7ce1b49534c54bbd51469f
Kirill Kononenko <kononenkokireall@gmail.com>
2025-01-27 16:46:18 +0100

    Create add product in cart for user

--- bot/handlers/user_private.py
+++ bot/handlers/user_private.py
@@ -21,8 +21,26 @@ async def start_cmd(message: types.Message, session: AsyncSession):
     await message.answer_photo(media.media, caption=media.caption, reply_markup=reply_markup)
 
 
+async def add_to_cart(callback: types.CallbackQuery, callback_data: MenuCallBack, session: AsyncSession):
+    user = callback.from_user
+    await orm_add_user(
+        session,
+        user_id=user.id,
+        first_name=user.first_name,
+        last_name=user.last_name,
+        phone=None,
+    )
+    await orm_add_to_cart(session, user_id=user.id, product_id=callback_data.product_id)
+    await callback.answer("Товар добавлен в корзину!")
+
+
+
 @user_private_router.callback_query(MenuCallBack.filter())
 async def user_menu(callback: types.CallbackQuery, callback_data: MenuCallBack, session: AsyncSession):
+
+    if callback_data.menu_name == "add_to_cart":
+        await add_to_cart(callback, callback_data, session)
+        return
 
     media, reply_markup = await get_menu_content(
         session,
