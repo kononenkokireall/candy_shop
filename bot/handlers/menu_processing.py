cae6f22adc1658ae4aee2e734f7b9561586185c3
Kirill Kononenko <kononenkokireall@gmail.com>
2025-01-27 16:34:00 +0100

    Create 2 level inline keyboard

--- bot/handlers/menu_processing.py
+++ bot/handlers/menu_processing.py
@@ -1,7 +1,16 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from aiogram.types import InputMediaPhoto
-from database.orm_query import orm_get_banner, orm_get_categories
-from keyboards.inline import get_user_main_btn, get_user_catalog_btn
+
+from database.orm_query import (
+    orm_get_banner,
+    orm_get_categories,
+    orm_get_products,
+    Paginator)
+
+from keyboards.inline import (
+    get_user_main_btn,
+    get_user_catalog_btn,
+    get_user_product_btn)
 
 
 async def main_menu(session, level, menu_name):
@@ -19,8 +28,48 @@ async def catalog(session, level, menu_name):
     return image, keyboards
 
 
-async def get_menu_content(session: AsyncSession, level: int, menu_name: str):
+def pages(paginator: Paginator):
+    btn = dict()
+    if paginator.has_previous():
+        btn["◀️Пред."] = "prev"
+    if paginator.has_next():
+        btn["След.▶️"] = "next"
+    return btn
+
+
+
+async def products(session, level, category, page):
+    products_user = await orm_get_products(session, category_id=category)
+    paginator = Paginator(products_user, page=page)
+    product_user = paginator.get_page()[0]
+    image = InputMediaPhoto(
+        media=product_user.image,
+        caption=f"<strong>{product_user.name}</strong>\n{product_user.description}\n"
+                f"Стоимость: {round(product_user.price, 2)}\n"
+                f"<strong>Товар {paginator.page} из {paginator.pages}</strong>",
+    )
+
+    pagination_btn = pages(paginator)
+    keyboards = get_user_product_btn(
+        level=level,
+        category=category,
+        page=page,
+        pagination_btn=pagination_btn,
+        product_id=product_user.id,
+    )
+
+    return image, keyboards
+
+async def get_menu_content(
+        session: AsyncSession,
+        level: int,
+        menu_name: str,
+        category: int | None = None,
+        page: int | None = None,
+):
     if level == 0:
         return await main_menu(session, level, menu_name)
     elif level == 1:
         return await catalog(session, level, menu_name)
+    elif level == 2:
+        return await products(session, level, category, page)
--- bot/keyboards/inline.py
+++ bot/keyboards/inline.py
@@ -6,6 +6,8 @@ class MenuCallBack(CallbackData, prefix="menu"):
     level: int
     menu_name: str
     category: int | None = None
+    page: int = 1
+    product_id: int | None = None
 
 
 def get_user_main_btn(*, level: int, sizes: tuple[int] = (2,)):
@@ -36,7 +38,7 @@ def get_user_main_btn(*, level: int, sizes: tuple[int] = (2,)):
 def get_user_catalog_btn(*, level: int, categories: list, sizes: tuple[int] = (2,)):
     keyboard = InlineKeyboardBuilder()
     keyboard.add(InlineKeyboardButton(
-        text="Назад", callback_data=MenuCallBack(level=level-1, menu_name='main').pack()
+        text="◀️Вернутся назад", callback_data=MenuCallBack(level=level-1, menu_name='main').pack()
     ))
     keyboard.add(InlineKeyboardButton(
         text="Корзина 🛒", callback_data=MenuCallBack(level=3, menu_name='cart').pack()
@@ -47,6 +49,52 @@ def get_user_catalog_btn(*, level: int, categories: list, sizes: tuple[int] = (2
             text=ctg.name, callback_data=MenuCallBack(level=level+1, menu_name=ctg.name, category=ctg.id).pack()
         ))
     return keyboard.adjust(*sizes).as_markup()
+
+
+def get_user_product_btn(
+        *,
+        level: int,
+        category: int,
+        page: int,
+        pagination_btn: dict,
+        product_id: int,
+        sizes: tuple[int] = (2, 1)
+):
+    keyboard = InlineKeyboardBuilder()
+    keyboard.add(InlineKeyboardButton(
+        text="◀️Вернутся назад", callback_data=MenuCallBack(level=level-1, menu_name='catalog').pack()
+    ))
+    keyboard.add(InlineKeyboardButton(
+        text="Корзина 🛒", callback_data=MenuCallBack(level=3, menu_name='cart').pack()
+    ))
+    keyboard.add(InlineKeyboardButton(
+        text="Купить 💵", callback_data=MenuCallBack(level=level, menu_name='add_to_cart', product_id=product_id).pack()
+    ))
+    keyboard.adjust(*sizes)
+
+    row = []
+    for text, menu_name in pagination_btn.items():
+        if menu_name == 'next':
+            row.append(InlineKeyboardButton(
+                text=text, callback_data=MenuCallBack(
+                    level=level,
+                    menu_name=menu_name,
+                    category=category,
+                    page=page+1).pack()
+            ))
+        elif menu_name == 'prev':
+            row.append(InlineKeyboardButton(
+                text=text, callback_data=MenuCallBack(
+                    level=level,
+                    menu_name=menu_name,
+                    category=category,
+                    page=page-1).pack()
+            ))
+    return keyboard.row(*row).as_markup()
+
+
+
+
 
 
 def get_callback_btn(*, btn: dict[str, str], sizes: tuple[int] = (2,)):
